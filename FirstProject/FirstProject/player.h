a5e56442cbf15c12537a13af4b28b0412c140971
Lorenzo Bottecchia <lorenzo.bottecchia982@escapestudios.ac.uk>
2026-09-25 10:19:31 +0100

    added player.h and weapon.h

---
+++ FirstProject/FirstProject/player.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "weapon.h"
+
+struct player
+{
+	int health;
+};
+
+#endif // PLAYER_H
---
+++ FirstProject/FirstProject/weapon.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#include "player.h"
+
+struct weapon
+{
+	int damage;
+};
+
+#endif // WEAPON_H
